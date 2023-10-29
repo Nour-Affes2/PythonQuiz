c68619b92e4a7b2653062b96bef79b3631ad2646
Nour Affes <135600882+Nour-Affes2@users.noreply.github.com>
2023-10-29 10:34:47 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,100 @@
+import random
+from string import ascii_lowercase
+import pathlib
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
+QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
+
+
+def prepare_questions(path, num_questions):
+    topic_info = tomllib.loads(path.read_text())
+    topics = {
+        topic["label"]: topic["questions"] for topic in topic_info
+    }
+    topic_label = get_answers(question="Which topic do you want to be quizzed about?",
+                              alternatives=sorted(topics),
+                              )[0]
+    questions = topics[topic_label]
+    num_questions = min(num_questions, len(questions))
+    return random.sample(questions, k=num_questions)
+
+
+def get_answers(question, alternatives, num_choices=1):
+    print(f"{question}?")
+    labeled_alternatives = dict(zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while True:
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nChoice{plural_s}?")
+        answers = set(answer.replace(",", " ").split())
+
+        # Handle invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+
+        if any(
+                (invalid := answer) not in labeled_alternatives
+                for answer in answers
+        ):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {','.join(labeled_alternatives)}"
+            )
+            continue
+
+        return [labeled_alternatives[answer] for answer in answers]
+
+
+# while (answer_label := input("\nChoice?")) not in labeled_alternatives:
+# print(f"Please answer one of {','.join(labeled_alternatives)}")
+# return labeled_alternatives[answer_label]
+
+
+def ask_question(question):
+    correct_answers = question["answers"]
+    alternatives = question["answers"] + question["alternatives"]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answers = get_answers(
+        question["question"],
+        ordered_alternatives,
+        num_choices=len(correct_answers)
+    )
+    if correct := (set(answers) == set(correct_answers)):
+        print("⭐ Correct! ⭐")
+    else:
+        is_or_are = " is" if len(correct_answers) == 1 else "s are"
+        print("\n- ".join([f"No, the answer{is_or_are}:"] + correct_answers))
+
+    if "explanation" in question:
+        print(f"\nEXPLANATION:\n{question['explanation']}")
+
+    return 1 if correct else 0
+
+
+def run_quiz():
+    questions = prepare_questions(
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
+
+    num = 0
+    num_correct = 0
+    for num, question in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question)
+
+    print(f"\nYou got {num_correct} correct out of {num} questions")
+
+
+if __name__ == "__main__":
+    run_quiz()
